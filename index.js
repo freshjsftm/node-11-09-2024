06d73141015ea2ed388ddb87c72dd72d529fdea6
Olena Zdanovska <freshjs.ftm@gmail.com>
2024-09-11 17:52:22 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log('hi from node!');
+const num1 = 5;
+const num2 = 8;
+const mult = (n1, n2) => n1 * n2;
+console.log(num1 + num2);
+console.log(mult(4,9));
+
+class Article{
+  constructor(content){
+    this.content = content;
+  }
+  render(){
+    return this.content;  
+  }
+}
+const newArticle = new Article('Content for article!');
+console.log(newArticle.render());
